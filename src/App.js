7f3cc80100089a7a7dcd8940a0a61591147477a1
Emmanuel252 <ouattarayoussoufemmanuel@gmail.com>
2023-03-31 00:24:31 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import Hero from './Component/Hero';
+import Navbar from './Component/Navbar';
+import About from './Component/About';
+import Services from './Component/Services';
+import Tours from './Component/Tours';
+import Footer from './Component/Footer';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <Services />
+      <Tours />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Component/Tours.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import Title from './Title';
+import { tours } from '../data';
+
+const Tours = () => {
+  return (
+    <section className="section" id="tours">
+      <Title title="featured" subTitle="Tours" />
+
+      <div className="section-center featured-center">
+        {tours.map(tour => {
+          const { id, img, date, title, text, icon, country, day, price } =
+            tour;
+          return (
+            <article className="tour-card" key={id}>
+              <div className="tour-img-container">
+                <img src={img} className="tour-img" alt="" />
+                <p className="tour-date">{date}</p>
+              </div>
+              <div className="tour-info">
+                <h4>{title}</h4>
+                <p>{text}</p>
+                <div className="tour-footer">
+                  <p>
+                    <span>
+                      <i className={icon}></i>
+                    </span>{' '}
+                    {country}
+                  </p>
+                  <p>{day}</p>
+                  <p>{price}</p>
+                </div>
+              </div>
+            </article>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default Tours;
